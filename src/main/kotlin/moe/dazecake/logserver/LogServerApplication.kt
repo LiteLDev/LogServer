dc38adef229337c86fae208a1a7080a0dbf77912
DazeCake <DazeCake@gmail.com>
2022-02-01 18:24:38 +0800

    :tada:Init project

---
+++ src/main/kotlin/moe/dazecake/logserver/LogServerApplication.kt
@@ -0,0 +1,11 @@
+package moe.dazecake.logserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<LogServerApplication>(*args)
+}
---
+++ src/test/kotlin/moe/dazecake/logserver/LogServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package moe.dazecake.logserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
